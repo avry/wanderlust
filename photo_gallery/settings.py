c4d57b4412ff792dcdd794ba99b56b4181c5ea64
Tan Avery <altan@ualberta.ca>
2018-03-29 01:03:03 -0600

    snapshot. app working locally

--- photo_gallery/settings.py
+++ photo_gallery/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-fdq2s)zvxa@+9l3nl9&(n$*+e@v)+ex(90hct*sodc7zm==xq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
