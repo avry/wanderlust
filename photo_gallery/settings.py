cde950a0c409c2fca804dadea514f437bb309c93
Tan Avery <altan@ualberta.ca>
2018-03-31 02:49:36 -0600

    added the correct host name

--- photo_gallery/settings.py
+++ photo_gallery/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['shrouded-plateau-10582.herokuapp.com']
+ALLOWED_HOSTS = ['intense-harbor-72744']
 
 
 # Application definition
