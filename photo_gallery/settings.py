4ace245753a1969d6f91874c9c2e109370609669
Tan Avery <altan@ualberta.ca>
2018-03-31 02:38:25 -0600

    removed django_heroku module

--- photo_gallery/settings.py
+++ photo_gallery/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,4 +130,4 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_files")
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
